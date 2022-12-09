c205a3008837ee6a9dd41a9b5ba979f93bc9d0c1
Luke Armitage <gillyobeast@gmail.com>
2022-12-09 21:47:00 +0000

    extract Matrix.kt

--- src/main/kotlin/aoc2022/utils/Utils.kt
+++ src/main/kotlin/aoc2022/utils/Utils.kt
@@ -26,45 +26,6 @@ fun <L, T> ((L) -> T).appliedTo(
 typealias Point2D<E> = Pair<E, E>
 typealias Point = Point2D<Int>
 
-typealias Matrix<E> = List<List<E>>
-
-fun <E> Matrix<E>.prettyPrint() {
-    println(joinToString("\n"))
-}
-
-fun matrixOf(input: List<String>): Matrix<Int> = input.map {
-    it.split("").filter(String::isNotBlank).map(String::toInt).toList()
-}
-
-
-fun <E> Matrix<E>.transposed(): Matrix<E> {
-    forEach { check(it.size == this[0].size) { "Non-square matrix passed to transpose" } }
-    val copy = mutableListOf<List<E>>()
-    for (i: Int in 0..this.lastIndex) {
-        val list = mutableListOf<E>()
-        for (j: Int in 0..this[i].lastIndex) {
-            list.add(j, this[j][i])
-        }
-        copy.add(list.toList())
-    }
-
-    return copy.toList()
-}
-
-operator fun <E> Matrix<E>.get(
-    rowIndex: Int,
-    colIndex: Int
-): Triple<List<E>, List<E>, E> {
-    val row = this[rowIndex]
-    val column = column(colIndex)
-    val tree = row[colIndex]
-    return Triple(row, column, tree)
-}
-
-
-fun <E> Matrix<E>.column(i: Int): List<E> {
-    return map { it[i] }.toList()
-}
 
 fun <T> T.shouldNotBe(equalTo: T): T {
     check(this != equalTo) { "Shouldn't be $equalTo" }
