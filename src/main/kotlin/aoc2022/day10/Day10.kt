de668ec347d0bc66854636a98b2bfc21dd50f941
Luke Armitage <gillyobeast@gmail.com>
2022-12-10 19:56:46 +0000

    day 10 - part 1

--- src/main/kotlin/aoc2022/day10/Day10.kt
+++ src/main/kotlin/aoc2022/day10/Day10.kt
@@ -7,12 +7,36 @@ import aoc2022.utils.readTestInput
 
 object Day10 : Puzzle(2022, 10) {
     override fun part1(input: List<String>): Int {
-        return input.size
+
+        var x = 1
+        var clock = 0
+        val checkpoints = setOf(20, 60, 100, 140, 180, 220)
+        val signalStrengths = mutableListOf<Int>()
+
+        input.forEach { instruction ->
+            if (instruction == "noop") {
+                clock++
+                if (clock in checkpoints) {
+                    signalStrengths.add(x * clock)
+                }
+            } else {
+                repeat(2) { _ ->
+                    clock++
+                    if (clock in checkpoints) {
+                        signalStrengths.add(x * clock)
+                    }
+                }
+                x += instruction.substringAfter(" ").toInt()
+            }
+        }
+
+        return signalStrengths.sum()
     }
 
     override fun part2(input: List<String>): Int {
         return input.size
     }
+
     fun solve() {
 
         val testInput = readTestInput()
