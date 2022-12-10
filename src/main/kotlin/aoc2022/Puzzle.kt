c132e9eb579cc31f85e1f226a729e595d03bd593
Luke Armitage <gillyobeast@gmail.com>
2022-12-10 18:05:19 +0000

    day 10 - set up and create Puzzle

---
+++ src/main/kotlin/aoc2022/Puzzle.kt
@@ -0,0 +1,10 @@
+package aoc2022
+
+abstract class Puzzle(val year: Int, val day:Int){
+    abstract fun solvePart1(input: String): Any
+    abstract fun solvePart2(input: String): Any
+}
+
+fun main(){
+    
+}
---
+++ src/main/kotlin/aoc2022/day10/Day10.kt
@@ -0,0 +1,28 @@
+package aoc2022.day10
+
+import aoc2022.utils.appliedTo
+import aoc2022.utils.readInput
+import aoc2022.utils.readTestInput
+
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
+
+fun main() {
+
+    val testInput = readTestInput()
+    val input = readInput()
+
+    // part 1
+    ::part1.appliedTo(testInput, returns = -1)
+    println("Part 1: ${part1(input)}")
+
+    // part 2
+    ::part2.appliedTo(testInput, returns = -1)
+    println("Part 2: ${part2(input)}")
+}
+
