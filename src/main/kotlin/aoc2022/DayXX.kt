b1deed6f2b11d828b4013afeec699c70a621dbce
Luke Armitage <gillyobeast@gmail.com>
2022-12-11 20:34:56 +0000

    more template fixes

---
+++ src/main/kotlin/aoc2022/DayXX.kt
@@ -0,0 +1,13 @@
+package aoc2022
+
+object DayXX: Puzzle(2022, 0){
+    override fun part1(input: List<String>): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: List<String>): Any {
+        TODO("Not yet implemented")
+    }
+
+}
+
