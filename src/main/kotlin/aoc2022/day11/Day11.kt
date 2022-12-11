fb8235aa1a9f0c12c8cc98403fbeb2e447ee5f35
Luke Armitage <gillyobeast@gmail.com>
2022-12-11 14:57:45 +0000

    day 11 - finish parsing monkeys

--- src/main/kotlin/aoc2022/day11/Day11.kt
+++ src/main/kotlin/aoc2022/day11/Day11.kt
@@ -20,32 +20,42 @@ fun String.toMonkey() =
 private fun String.parseNum() = substringBefore(":").toInt()
 
 private fun String.parseIfFalse(): Int {
-    TODO("Not yet implemented")
-
+    return extractInt("If false: throw to monkey (\\d+)".toRegex())
 }
 
 private fun String.parseIfTrue(): Int {
-    TODO("Not yet implemented")
-
+    return extractInt("If true: throw to monkey (\\d+)".toRegex())
 }
 
-private fun String.parseTest(): Int {
-    TODO("Not yet implemented")
 
+private fun String.parseTest(): Int {
+    return extractInt("Test: divisible by (\\d+)".toRegex())
 }
 
 fun String.parseOperation(): (Int) -> Int {
-    TODO("Not yet implemented")
+    val operation = extract("Operation: new = old ([*+]) (\\d+|old)".toRegex())
+    val operand = extract("Operation: new = old [*+] (\\d+|old)".toRegex())
+
+    return if (operand == "old") { it -> it * it }
+    else if (operation == "*") { it -> it * operand.toInt() }
+    else { it -> it + operand.toInt() }
 }
 
-private fun String.parseItems() =
-    "Starting items: ((\\d+,) )+".toRegex().find(this)!!.value.split(",").map(String::toInt)
+private fun String.parseItems(): List<Int> =
+    extract("Starting items: ((\\d+,?)\\s?)+".toRegex()).split(",").map(String::toInt)
+
+private fun String.extractInt(regex: Regex): Int {
+    return extract(regex).toInt()
+}
+
+private fun String.extract(regex: Regex): String =
+    regex.find(this)!!.destructured.component1().trim()
 
 object Day11 : Puzzle(2022, 11) {
     override fun part1(input: List<String>): Any {
         input.joinToString("")
             .split("Monkey".toRegex()).drop(1)
-            .map { it.trim().log() }
+            .map { it.trim().toMonkey().andLog() }
 
         TODO("Return something, dummy!")
     }
@@ -55,7 +65,7 @@ object Day11 : Puzzle(2022, 11) {
     }
 }
 
-private fun <T> T.log(): T = this.also { println(it) }
+private fun <T> T.andLog(): T = this.also { println(it) }
 
 
 fun main() {
