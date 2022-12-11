3afc85ffe53bc233159b23f8573db9803e264cd5
Luke Armitage <gillyobeast@gmail.com>
2022-12-11 11:04:40 +0000

    day 11 - continue parsing monkeys

--- src/main/kotlin/aoc2022/day11/Day11.kt
+++ src/main/kotlin/aoc2022/day11/Day11.kt
@@ -4,13 +4,47 @@ import aoc2022.Puzzle
 
 data class Monkey(
     val num: Int, val items: List<Int>, val operation: (Int) -> Int,
-    val ifTrue: Int, val ifFalse: Int,
+    val testDivisibleBy: Int, val ifTrueTarget: Int, val ifFalseTarget: Int,
 )
+
+fun String.toMonkey() =
+    Monkey(
+        num = parseNum(),
+        items = parseItems(),
+        operation = parseOperation(),
+        testDivisibleBy = parseTest(),
+        ifTrueTarget = parseIfTrue(),
+        ifFalseTarget = parseIfFalse()
+    )
+
+private fun String.parseNum() = substringBefore(":").toInt()
+
+private fun String.parseIfFalse(): Int {
+    TODO("Not yet implemented")
+
+}
+
+private fun String.parseIfTrue(): Int {
+    TODO("Not yet implemented")
+
+}
+
+private fun String.parseTest(): Int {
+    TODO("Not yet implemented")
+
+}
+
+fun String.parseOperation(): (Int) -> Int {
+    TODO("Not yet implemented")
+}
+
+private fun String.parseItems() =
+    "Starting items: ((\\d+,) )+".toRegex().find(this)!!.value.split(",").map(String::toInt)
 
 object Day11 : Puzzle(2022, 11) {
     override fun part1(input: List<String>): Any {
         input.joinToString("")
-            .split("Monkey \\d+:".toRegex()).drop(1)
+            .split("Monkey".toRegex()).drop(1)
             .map { it.trim().log() }
 
         TODO("Return something, dummy!")
