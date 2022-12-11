c88fba91a0fb64dab15163ae5c19e456167f44b0
Luke Armitage <gillyobeast@gmail.com>
2022-12-11 10:44:32 +0000

    day 11 - start parsing monkeys

--- src/main/kotlin/aoc2022/day11/Day11.kt
+++ src/main/kotlin/aoc2022/day11/Day11.kt
@@ -2,17 +2,30 @@ package aoc2022.day11
 
 import aoc2022.Puzzle
 
-object Day11 : Puzzle(2022,11) {
+data class Monkey(
+    val num: Int, val items: List<Int>, val operation: (Int) -> Int,
+    val ifTrue: Int, val ifFalse: Int,
+)
+
+object Day11 : Puzzle(2022, 11) {
     override fun part1(input: List<String>): Any {
-        TODO("Not yet implemented")
+        input.joinToString("")
+            .split("Monkey \\d+:".toRegex()).drop(1)
+            .map { it.trim().log() }
+
+        TODO("Return something, dummy!")
     }
 
     override fun part2(input: List<String>): Any {
         TODO("Do part 1 first!")
     }
 }
+
+private fun <T> T.log(): T = this.also { println(it) }
+
+
 fun main() {
-    Day11.solve(10605,-1)
+    Day11.solve(10605, -1)
 }
 
 
