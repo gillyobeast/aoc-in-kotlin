3ab640ec88f0c04783c3ff3172f83288f0aefb08
Luke Armitage <gillyobeast@gmail.com>
2022-12-12 19:52:46 +0000

    day12 parse input

--- src/main/kotlin/aoc2022/day12/Day12.kt
+++ src/main/kotlin/aoc2022/day12/Day12.kt
@@ -1,9 +1,20 @@
 package aoc2022.day12
 
 import aoc2022.Puzzle
+import aoc2022.utils.Matrix
 
 object Day12 : Puzzle(2022, 12) {
+    /**
+     * Strategy:
+     *      - parse input
+     *      - find a path (somehow)
+     *      - tweak the path, check its length and keep if it's shorter
+     *
+     */
     override fun part1(input: List<String>): Any {
+        val map: Matrix<Char> = input.map { it.toList() }
+
+
         TODO("Not yet implemented")
     }
 
