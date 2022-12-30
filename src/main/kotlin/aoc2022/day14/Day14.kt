4e76c075c83623066e50752ab4739bed9c2f3a8e
Luke Armitage <gillyobeast@gmail.com>
2022-12-30 17:44:36 +0000

    day 14 - move sand through cave

--- src/main/kotlin/aoc2022/day14/Day14.kt
+++ src/main/kotlin/aoc2022/day14/Day14.kt
@@ -1,7 +1,6 @@
 package aoc2022.day14
 
 import aoc2022.Puzzle
-import aoc2022.utils.andLog
 import aoc2022.utils.prettyPrint
 import kotlin.reflect.KProperty1
 
@@ -31,7 +30,7 @@ object Day14 : Puzzle(2022, 14) {
         val updateMinY = { it: Int -> if (maxY < it) maxY = it }
         var (minX, maxX) = 500 to 500
         val updateXRange = { it: Int -> if (it < minX) minX = it; if (maxX < it) maxX = it }
-        input.parsePoints().andLog()
+        input.parsePoints()/*.andLog()*/
             .forEach { points ->
                 points.windowed(2) { (from, to) ->
                     for (point in pointsBetween(from, to)) {
@@ -46,14 +45,12 @@ object Day14 : Puzzle(2022, 14) {
         println("maxX = ${maxX}")
         println("maxY = ${maxY}")
 
-        println(rocks)
-
         val cave: MutableList<MutableList<Char>> = mutableListOf()
         // rows then columns
         repeat(maxY) { rowIdx ->
             val row = mutableListOf<Char>()
             repeat(maxX - minX) { colIdx ->
-                if (Point(colIdx+1 + minX, rowIdx+1).andLog() in rocks) {
+                if (Point(colIdx + 1 + minX, rowIdx + 1) in rocks) {
                     row.add(ROCK)
                 } else row.add(AIR)
             }
@@ -62,10 +59,23 @@ object Day14 : Puzzle(2022, 14) {
         cave.prettyPrint()
 
 
+        untilFalse {
+            // in this loop, one unit of sand is moved through the cave.
+
+
+            true
+        }
 
 
 
-        TODO("Not yet implemented")
+        return 0
+    }
+
+    private fun untilFalse(limit: Int = 10_000, block: (Int) -> Boolean) {
+        var count = 0
+        @Suppress("ControlFlowWithEmptyBody")
+        while (block(count) && count++ > limit) {
+        }
     }
 
     private fun pointsBetween(from: Point, to: Point): Set<Point> {
