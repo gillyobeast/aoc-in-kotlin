182b4e2cd3c7523465636895eeb14c4267c7723d
Luke Armitage <gillyobeast@gmail.com>
2022-12-31 22:38:19 +0000

    day 14 - part 2 done - hot mess but it works

--- src/main/kotlin/aoc2022/day14/Day14.kt
+++ src/main/kotlin/aoc2022/day14/Day14.kt
@@ -12,7 +12,7 @@ const val SAND = 'x'
 fun <E> Matrix<E>.prettyPrint(path: List<Point>, minX: Int) {
     println(mapIndexed { rowIndex, row ->
         row.mapIndexed { colIndex, value ->
-            (if (path.contains(Point(colIndex + minX, rowIndex))) ">" else " ") + value.toString()
+            (if (path.contains(Point(colIndex + minX, rowIndex))) ">" else "") + value.toString()
         }.joinToString("")
     }.joinToString("\n"))
 }
@@ -55,7 +55,8 @@ object Day14 : Puzzle(2022, 14) {
         var sands = 0
         outer@ while (sands++ < 10_000) {
             // in this loop, one unit of sand is moved through the cave.
-            var sand = Point(499, 0)
+            val start = Point(499, 0)
+            var sand = start
             val path = mutableListOf(sand)
             var moves = 0
             while (moves++ < maxY) {
@@ -164,9 +165,12 @@ object Day14 : Puzzle(2022, 14) {
     }
 
     override fun part2(input: List<String>): Any {
-        val (xTmp, yTmp) = extracted(input)
-        val (minX, maxX) = xTmp
-        val (maxY, rocks) = yTmp
+        val (_, yTmp) = extracted(input)
+        var (maxY, rocks) = yTmp
+        maxY+=2
+        val minX = 250
+        val maxX = 750
+        rocks.addAll(pointsBetween(Point(0, maxY ), Point(1000, maxY)))
 
         val cave: MutableList<MutableList<Char>> = mutableListOf()
         // rows then columns
@@ -181,58 +185,65 @@ object Day14 : Puzzle(2022, 14) {
         }
 //        cave.prettyPrint()
         var sands = 0
-        outer@ while (sands++ < 10_000) {
-            // in this loop, one unit of sand is moved through the cave.
-            var sand = Point(499, 0)
-            val path = mutableListOf(sand)
-            var moves = 0
-            while (moves++ < maxY) {
-                // in this loop, the unit of sand moves down one unit if it can,
-                //  moving diagonally as necessary
-                var xCandidate = sand.x
-                val yCandidate = sand.y + 1
-                if (yCandidate >= maxY) {
-                    // we're done - send next sand
-                    continue@outer
-                }
-                // try below
-                val row = cave[yCandidate]
-                if (row.getOrNull(xCandidate - minX) !in rockOrSand) {
-                    // if not blocked
-                    sand = Point(xCandidate, yCandidate)
-                    path.add(sand)
-                    continue
-                }
-                // try diagonally left
-                xCandidate = sand.x - 1
-                if (row.getOrNull(xCandidate - minX) !in rockOrSand) {
-                    sand = Point(xCandidate, yCandidate)
-                    path.add(sand)
-                    continue
-                }
-                // try diagonally right
-                xCandidate = sand.x + 1
-                if (row.getOrNull(xCandidate - minX) !in rockOrSand) {
-                    sand = Point(xCandidate, yCandidate)
-                    path.add(sand)
-                    continue
-                }
+        try{
+            outer@ while (sands++ < 1_000_000) {
+                // in this loop, one unit of sand is moved through the cave.
+                val inlet = Point(499, -1)
+                var sand = inlet
+                val path = mutableListOf(sand)
+                var moves = 0
+                while (moves++ < maxY) {
+                    // in this loop, the unit of sand moves down one unit if it can,
+                    //  moving diagonally as necessary
+                    val yCandidate = sand.y + 1
+                    if (yCandidate >= maxY || (moves > 1 && sand == inlet)) {
+                        // we're done - send next sand
+                        continue@outer
+                    }
+                    var xCandidate = sand.x
+                    val row = cave[yCandidate]
+                    // try below
+                    if (row.getOrNull(xCandidate - minX) !in rockOrSand) {
+                        // if not blocked
+                        sand = Point(xCandidate, yCandidate)
+                        path.add(sand)
+                        continue
+                    }
+                    // try diagonally left
+                    xCandidate = sand.x - 1
+                    if (row.getOrNull(xCandidate - minX) !in rockOrSand) {
+                        sand = Point(xCandidate, yCandidate)
+                        path.add(sand)
+                        continue
+                    }
+                    // try diagonally right
+                    xCandidate = sand.x + 1
+                    if (row.getOrNull(xCandidate - minX) !in rockOrSand) {
+                        sand = Point(xCandidate, yCandidate)
+                        path.add(sand)
+                        continue
+                    }
 
-                cave[yCandidate - 1][sand.x - minX] = SAND
-                break
+                    cave[yCandidate - 1][sand.x - minX] = SAND
+                    break
+                }
+//                println("-".repeat((maxX - minX) * 2 - 1))
+//                cave.prettyPrint(path, minX)
+                if (sand.y > maxY)
+                    break
             }
-//            println("-".repeat((maxX - minX) * 2 - 1))
-//            cave.prettyPrint(path, minX)
-            if (sand.y > maxY)
-                break
+        }catch (e:Throwable){
+            println(e)
         }
 
         println("-".repeat((maxX - minX) * 2 - 1))
         cave.prettyPrint(mutableListOf(), minX)
-        return cave.sumOf { row -> row.count { it == SAND } } shouldNotBe 127 shouldNotBe 863    }
+        return cave.sumOf { row -> row.count { it == SAND } } +1
+    }
 }
 
 fun main() {
     Day14.solve(24, 93)
 }
 
+//ooooooooo
