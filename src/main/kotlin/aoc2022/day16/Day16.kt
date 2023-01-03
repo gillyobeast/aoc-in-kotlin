f04585cc7bbcf95bdd1be85170c63c4e1dd65240
Luke Armitage <gillyobeast@gmail.com>
2023-01-03 09:24:13 +0000

    Day 16 part 1 - parsing

--- src/main/kotlin/aoc2022/day16/Day16.kt
+++ src/main/kotlin/aoc2022/day16/Day16.kt
@@ -1,10 +1,28 @@
 package aoc2022.day16
 
 import aoc2022.Puzzle
+import aoc2022.utils.andLog
 
 object Day16 : Puzzle(2022, 0) {
+    private data class Valve(val flowRate: Int, val leadsTo: Set<String>)
+
+    private fun String.parse(): Pair<String, Valve> {
+        val label = substringAfter("Valve ").substringBefore(" has")
+        val flowRate = substringAfter("=").substringBefore(";").toInt()
+        val leadsTo = substringAfter("valve").removePrefix("s")
+            .removePrefix(" ").split(", ").toSet()
+        return label to Valve(flowRate, leadsTo)
+    }
+
+
+    private fun List<String>.parse(): Map<String, Valve> =
+        associate { it.parse() }
+
     override fun part1(input: List<String>): Any {
-        TODO("Not yet implemented")
+        input.parse().andLog{it.entries.joinToString("\n")}
+        println()
+
+        TODO()
     }
 
     override fun part2(input: List<String>): Any {
@@ -13,6 +31,6 @@ object Day16 : Puzzle(2022, 0) {
 
 }
 
-fun main() = Day16.solve(-1, -1)
+fun main() = Day16.solve(1651, -1)
 
 
