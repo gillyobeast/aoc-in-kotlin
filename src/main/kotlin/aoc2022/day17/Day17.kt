c9a871de7f851aeb4404d7efde20ae37d16a21df
Lake Armitage <gillyobeast@gmail.com>
2023-11-23 21:47:45 +0000

    wip: day 17, a bit of a start

--- src/main/kotlin/aoc2022/day17/Day17.kt
+++ src/main/kotlin/aoc2022/day17/Day17.kt
@@ -3,7 +3,15 @@ package aoc2022.day17
 import aoc2022.Puzzle
 
 object Day17 : Puzzle(2022, 0) {
+    private val rocks = listOf("####", ".#.\n###\n.#.", "..#\n..#\n###", "#\n#\n#\n#", "##\n##")
+    private const val NUMBER_OF_ROCKS = 2022
+
     override fun part1(input: List<String>): Any {
+        for (rockNumber in 1..NUMBER_OF_ROCKS) {
+            val rock = rocks(rockNumber)
+            println(rock)
+        }
+
         TODO("Not yet implemented")
     }
 
@@ -11,8 +19,9 @@ object Day17 : Puzzle(2022, 0) {
         TODO("Not yet implemented")
     }
 
+    operator fun <T> List<T>.invoke(int: Int) = get(int % size)
 }
 
-fun main() = Day17.solve(-1, -1)
+fun main() = Day17.solve(3068, -1)
 
 
