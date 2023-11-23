9d4e22a1390d53cbbda3657f55d6583ad5051a22
Lake Armitage <gillyobeast@gmail.com>
2023-11-23 21:17:35 +0000

    wip: day 16, it would probably work if i gave it three hours

--- src/main/kotlin/aoc2022/day16/Day16.kt
+++ src/main/kotlin/aoc2022/day16/Day16.kt
@@ -5,41 +5,51 @@ import aoc2022.utils.andLog
 import org.jgrapht.graph.DefaultDirectedGraph
 import org.jgrapht.graph.DefaultEdge
 import org.jgrapht.graph.builder.GraphBuilder
+import kotlin.coroutines.coroutineContext
 
 object Day16 : Puzzle(2022, 0) {
-    private data class Valve(val label: String, val flowRate: Int, val leadsTo: Set<String>) {
-        var isOpen = false
-    }
+    private data class Valve(val label: String, val flowRate: Long, val leadsTo: Set<String>)
 
     private fun String.parse(): Valve {
         val label = substringAfter("Valve ").substringBefore(" has")
-        val flowRate = substringAfter("=").substringBefore(";").toInt()
-        val leadsTo = substringAfter("valve").removePrefix("s")
-            .removePrefix(" ").split(", ").toSet()
+        val flowRate = substringAfter("=").substringBefore(";").toLong()
+        val leadsTo = substringAfter("valve").removePrefix("s").removePrefix(" ").split(", ").toSortedSet()
         return Valve(label, flowRate, leadsTo)
     }
 
 
-    private fun List<String>.parse(): Map<String, Valve> =
-        map { it.parse() }.associateBy { it.label }
+    private fun List<String>.parse(): Set<Valve> = map { it.parse() }.toSortedSet(compareBy { it.flowRate })
 
     override fun part1(input: List<String>): Any {
-        val valves = input.parse()//.andLog { it.entries.joinToString("\n") }
-
-        val start = valves["AA"] ?: error("No AA found.")
-
-        val graph = buildGraph(valves)
-        graph.edgeSet().andLog { it.joinToString("\n") }
-        println()
+        val valves = input.parse().andLog { it.joinToString("\n") }
 
 
+        return maximiseFlow(getStart(valves), valves, emptySet(), 0, 30)
 
-        TODO()
     }
 
+    private fun maximiseFlow(
+        current: Valve, closedValves: Set<Valve>, openValves: Set<Valve>, pressureReleased: Long, timeLeft: Int
+    ): Long {
+        if (timeLeft > 10 && pressureReleased > 100)
+            println("current = [${current}], pressureReleased = [${pressureReleased}], timeLeft = [${timeLeft}]")
+
+        if (timeLeft == 0) return pressureReleased
+        if (closedValves.isEmpty()) return (openValves.sumOf { it.flowRate } * timeLeft) + pressureReleased
+        val pressureToNow = openValves.sumOf { it.flowRate } + pressureReleased
+
+        return buildSet {
+            closedValves.parallelStream().forEach {
+                add(maximiseFlow(it, closedValves, openValves, pressureToNow, timeLeft - 1))
+            }
+            add(maximiseFlow(current, closedValves - current, openValves + current, pressureToNow, timeLeft - 1))
+        }.max()
+    }
+
+    private fun getStart(valves: Set<Valve>) = valves.first { it.label == "AA" }
+
     private fun buildGraph(valves: Map<String, Valve>): DefaultDirectedGraph<Valve, DefaultEdge> {
-        val builder =
-            GraphBuilder(DefaultDirectedGraph<Valve, DefaultEdge>(DefaultEdge::class.java))
+        val builder = GraphBuilder(DefaultDirectedGraph<Valve, DefaultEdge>(DefaultEdge::class.java))
         for (valve in valves.values) {
             for (other in valve.leadsTo) {
                 builder.addEdge(valve, valves[other])
