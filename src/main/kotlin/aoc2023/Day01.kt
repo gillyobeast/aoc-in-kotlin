f76704badaa1fd76d63b8ec68467281b233cea1a
Luke Armitage <gillyobeast@gmail.com>
2023-12-21 14:22:10 -0500

    Day 1: add file

---
+++ src/main/kotlin/aoc2023/Day01.kt
@@ -0,0 +1,18 @@
+package aoc2023
+
+import Puzzle
+
+object Day01: Puzzle(2023,1) {
+    override fun part1(input: List<String>): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: List<String>): Any {
+        TODO("Not yet implemented")
+    }
+
+}
+
+fun main() {
+    Day01.solve(142, -1)
+}
