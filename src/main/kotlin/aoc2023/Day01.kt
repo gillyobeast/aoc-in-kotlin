804e5914e8db8bffe8738d558c5d04a4f10fe2e5
Luke Armitage <gillyobeast@gmail.com>
2023-12-21 14:40:04 -0500

    Day 1: part 1

--- src/main/kotlin/aoc2023/Day01.kt
+++ src/main/kotlin/aoc2023/Day01.kt
@@ -2,10 +2,22 @@ package aoc2023
 
 import Puzzle
 
-object Day01: Puzzle(2023,1) {
+object Day01 : Puzzle(2023, 1) {
+    // The newly-improved calibration document consists of lines of text; each line originally
+    //  contained a specific calibration value that the Elves now need to recover. On each line,
+    //  the calibration value can be found by combining the first digit and the last digit
+    //  (in that order) to form a single two-digit number.
+    //  What is the sum of all of the calibration values?
+
     override fun part1(input: List<String>): Any {
-        TODO("Not yet implemented")
+        return input.map { it.extractDigits() }.map {
+            it.first() + it.last() // string concat
+        }.sumOf { it.toInt() }
+
     }
+
+    private fun String.extractDigits() =
+        filter { char -> char.isDigit() }.map { char -> char.toString() }
 
     override fun part2(input: List<String>): Any {
         TODO("Not yet implemented")
