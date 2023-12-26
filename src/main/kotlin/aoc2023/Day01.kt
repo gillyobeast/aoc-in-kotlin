9fa1b7eb1581e878d5eb9715cb781157621485ad
Luke Armitage <gillyobeast@gmail.com>
2023-12-26 16:47:27 -0500

    Day 1: part 2

--- src/main/kotlin/aoc2023/Day01.kt
+++ src/main/kotlin/aoc2023/Day01.kt
@@ -20,11 +20,34 @@ object Day01 : Puzzle(2023, 1) {
         filter { char -> char.isDigit() }.map { char -> char.toString() }
 
     override fun part2(input: List<String>): Any {
-        TODO("Not yet implemented")
+        return input.map { it: String -> it.extractNumbers() }.filter { it.isNotEmpty() }.map {
+            it.first() + it.last() // string concat
+        }.sumOf { it.toInt() }
+        
+    }
+
+    private val numbers = mapOf(
+        "one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5,
+        "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9,
+        "1" to 1, "2" to 2, "3" to 3, "4" to 4, "5" to 5,
+        "6" to 6, "7" to 7, "8" to 8, "9" to 9,
+    )
+
+    private fun String.extractNumbers(): List<String> {
+        val foundNumbers = mutableListOf<Int>()
+
+        for (idx in this.indices) {
+            for ((number, value) in numbers) {
+                if (this.indexOf(number, idx) == idx) {
+                    foundNumbers += value
+                }
+            }
+        }
+        return foundNumbers.map { it.toString() }
     }
 
 }
 
 fun main() {
-    Day01.solve(209, -1)
+    Day01.solve(209, 281)
 }
